659272ebc5933b7a390051ff83858dcda97114c1
Edward Prendergast <edward@adaodesign.com>
2021-06-25 20:34:19 +0100

    Can now upload files
    Like this:
    
    ```sh
    curl -v localhost:3000 -F file=@/tmp/783px-Test-Logo.svg.png
    ```

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to imgor
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/imgor/core.clj
@@ -0,0 +1,55 @@
+(ns imgor.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io])
+  (:gen-class))
+
+(def server-config
+  {:params {:urlencoded true,
+     :multipart true,
+     :nested true,
+     :keywordize true},
+     :cookies false,
+     :session {:flash true, :cookie-attrs {:http-only true, :same-site :strict}},
+    ; TODO: enable anti-forgery and xss-protection
+     :security {:anti-forgery false, :xss-protection {:enable? false, :mode :block}, :frame-options :sameorigin, :content-type-options :nosniff},
+     :static {:resources "public"},
+     :responses {:not-modified-responses true, :absolute-redirects true, :content-types true, :default-charset "utf-8"}})
+
+(defn save-file [req]
+  (let [tmpfilepath (:path (bean (get-in req [:params :file :tempfile])))
+        custom-path "resources/uploads/file.png"]
+  (do
+    (io/copy (io/file tmpfilepath) (io/file custom-path))
+    {:status 200
+     :headers  {"Content-Type" "text/html"}
+     :body (str "File now available for download at: http://localhost:3000/" custom-path)})))
+
+(defn index-handler
+  [_]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body    (slurp "resources/index.html")})
+
+(defroutes app-routes
+  (GET "/" [] index-handler)
+  (POST "/" [] save-file)
+  (route/not-found "Error, page not found!"))
+
+(defn -main
+  "Upload images to share on the web"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    (do
+      (server/run-server (wrap-defaults #'app-routes server-config) {:port port})
+      (println "Server started on port" port)
+      )))
+    ; Run the server with Ring.defaults middleware
+    
